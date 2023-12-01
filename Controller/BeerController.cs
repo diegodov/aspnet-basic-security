cab1949e3121047c9b8702ff7313cb923d855b6f
diegodov <diegodd07@gmail.com>
2023-12-01 16:44:51 -0300

    Add beer controller

--- Controller/BeerController.cs
+++ Controller/BeerController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BasicSecurityASP.Models;
+using BasicSecurityASP.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BasicSecurityASP.Controller
@@ -7,7 +9,15 @@ namespace BasicSecurityASP.Controller
     [ApiController]
     public class BeerController : ControllerBase
     {
+        private readonly IBeerService _beerService;
 
+        public BeerController(IBeerService beerService)
+        {
+            _beerService = beerService;
+        }
+
+        [HttpGet]
+        public async Task<List<Beer>> GetBeers() => await _beerService.GetBeers();
 
     }
 }
