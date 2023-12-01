1aba3c2589a3c5e8b8792689a17731d27fd915d8
Diego Dominguez <diegodd07@gmail.com>
2023-12-01 20:08:17 -0300

    Fix service login method

--- Services/UserService.cs
+++ Services/UserService.cs
@@ -7,14 +7,11 @@ namespace BasicSecurityASP.Services
 
         List<User> users = new List<User>()
         {
-            new User() {Email = "admin", Password = "admin"},
-            new User() {Email = "user", Password = "user"}
+            new User() {Email = "pato@hotmail.com", Password = "123456"},
+            new User() {Email = "pato2@hotail.com", Password = "123456"}
         }; 
 
-        public bool Login(string email, string password)
-        {   
-            users.Where(d => d.Email == email && d.Password == password).FirstOrDefault();
-            throw new NotImplementedException();
-        }
+        public bool Login(string email, string password) =>  
+          users.Where(d => d.Email == email && d.Password == password).Count() > 0;
     }
 }
